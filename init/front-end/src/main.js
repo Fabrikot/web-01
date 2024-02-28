7fd93547740cdc05a25809a7dd0a9882e7b38470
Fabrikot <fabien.devillechabrolle@epfedu.fr>
2024-02-28 15:19:53 +0100

    Ajout CSS loader part1

--- init/front-end/src/main.js
+++ init/front-end/src/main.js
@@ -1,10 +1,13 @@
 // TODO #import-router: use ES named imports to import the router
-import {Router} from "./router";
+import {Router} from "./app/scripts/router";
 // TODO #import-components: use ES named imports to import WelcomeComponent, GameComponent a ScoreComponent
-import {WelcomeComponent} from "./welcome";
-import {GameComponent} from "./game";
-import {ScoreComponent} from "./score";
+import {WelcomeComponent} from "./app/scripts/welcome";
+import {GameComponent} from "./app/scripts/game";
+import {ScoreComponent} from "./app/scripts/score";
 // TODO #import-css: use ES side-effect imports to import styles/style.css
+
+import "./app/styles/bootstrap.css";
+import "./app/styles/style.css";
 
 const outlet = document.querySelector("#content-outlet");
 const router = new Router(outlet);
--- init/front-end/webpack.config.js
+++ init/front-end/webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
   module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    entry: "./src/app/scripts/main.js",
+    entry: "./src/main.js",
     devServer: {
       static: {
         directory: path.join(__dirname),
@@ -19,6 +19,11 @@ const path = require("path");
       minimize: false,
     },
     module: {
-      rules: [],
-    },
+        rules: [
+          {
+            test: /\.(css)$/,
+            use: ["style-loader", "css-loader"],
+        },
+      ],
+    }
   };    
